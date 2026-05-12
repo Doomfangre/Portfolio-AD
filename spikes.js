fd5be5ddb9a46f31d94b6c2b37536ffa2fbb90e9
Doomfangre <Doomfangre@gmail.com>
2026-05-11 22:53:15 -0700

    Portugal

--- spikes.js
+++ spikes.js
@@ -1,5 +1,48 @@
-document.getElementById("portu").addEventListener("click", bite);
+document.getElementById("domer").addEventListener("click", inde);
+document.getElementById("pause").addEventListener("click", pause);
+document.getElementById("call").addEventListener("click", call);
+document.getElementById("portu").addEventListener("click", port);
+const s2 = document.querySelector('#s2');
+const s1 = document.querySelector('#s1');
+const grey2 = document.querySelector('#grey2');
+const grey1 = document.querySelector('#grey1');
 
-function bite() {
-    document.getElementById("portu")
+function port(){
+    s2.classList.toggle('is-reversed');
+    s1.classList.toggle('is-reversed');
+    grey2.classList.toggle('is-reversed');
+    grey1.classList.toggle('is-reversed');
+    setTimeout(() => {
+        window.location.href = "Port.html";
+    }, 1200);
+}
+
+function inde(){
+    s2.classList.toggle('is-reversed');
+    s1.classList.toggle('is-reversed');
+    grey2.classList.toggle('is-reversed');
+    grey1.classList.toggle('is-reversed');
+    setTimeout(() => {
+        window.location.href = "index.html";
+    }, 1200);
+}
+
+function pause(){
+    s2.classList.toggle('is-reversed');
+    s1.classList.toggle('is-reversed');
+    grey2.classList.toggle('is-reversed');
+    grey1.classList.toggle('is-reversed');
+    setTimeout(() => {
+        window.location.href = "Resume.html";
+    }, 1200);
+}
+
+function call(){
+    s2.classList.toggle('is-reversed');
+    s1.classList.toggle('is-reversed');
+    grey2.classList.toggle('is-reversed');
+    grey1.classList.toggle('is-reversed');
+    setTimeout(() => {
+        window.location.href = "Contact.html";
+    }, 1200);
 }
